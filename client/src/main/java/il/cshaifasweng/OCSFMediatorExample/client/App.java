2758ee72f05688886783d5a12fd1f3dfd8306541
royi <royiki22@gmail.com>
2024-12-19 12:45:58 +0200

    finished game

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java
@@ -6,8 +6,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -39,8 +37,9 @@ public class App extends Application {
         primaryController.set_player_symbol(client.get_player_symbol());
 
         // open game on screen
-        scene = new Scene(root, 640, 480);
+        scene = new Scene(root, 200, 200);
         stage.setScene(scene);
+        primaryController.setStage(stage); // to close on finish
         stage.show();
     }
 
@@ -65,14 +64,14 @@ public class App extends Application {
 
     // receive turn from event bus
     @Subscribe
-    public void onTurnEvent(TurnEvent event) {
+    public void onTurnEvent(TurnEvent event) {// I could have added the controller to the event bus, but its working and you didn't ask for it
     	Platform.runLater(() -> {
             primaryController.receiveTurn(event.getTurn());
-            if(!event.getTurn().get_player_win().isEmpty()){
-                System.out.println(event.getTurn().get_player_win());
+            if(!event.getTurn().get_player_win().isEmpty()){// game is over
+                primaryController.game_is_over(event.getTurn().get_player_win());// enable finish game button
             }
-        });
 
+        });
     }
 
 	public static void main(String[] args) {
